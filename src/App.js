1867d6beb134387914e37f3a07d2d12cfc8704c8
Alexnorvag <norvag@mail.ru>
2022-06-26 20:57:17 +0400

    added: text selecting styling

--- src/App.js
+++ src/App.js
@@ -18,19 +18,20 @@ function App() {
   });
 
   return (
-    <div className="w-full min-h-screen flex items-center justify-center bg-almost-black font-montserrat">
-      {letters.map((letter, idx) => (
-        <span
-          key={idx}
-          className={clsx(
-            "text-jungle-green uppercase text-xl sm:text-4xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl",
-            `${idx < loadedPos ? "bright" : "dim"}-glow`,
-            { "animate-pulse": isHacked }
-          )}
-        >
-          {letter}
-        </span>
-      ))}
+    <div className="w-full min-h-screen flex items-center justify-center bg-almost-black font-montserrat selection:bg-jungle-green selection:text-almost-black">
+      <div className={clsx({ "animate-pulse": isHacked })}>
+        {letters.map((letter, idx) => (
+          <span
+            key={idx}
+            className={clsx(
+              "text-jungle-green uppercase text-xl sm:text-4xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl",
+              `${idx < loadedPos ? "bright" : "dim"}-glow`
+            )}
+          >
+            {letter}
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
